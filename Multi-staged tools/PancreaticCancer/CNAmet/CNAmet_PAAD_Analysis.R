6b372a24a6f73cc726f47efd73c1e4380ebd43f6
AtinaSat <45379687+AtinaSat@users.noreply.github.com>
2019-01-29 12:53:34 +1000

    Add files via upload

---
+++ Multi-staged tools/PancreaticCancer/CNAmet/CNAmet_PAAD_Analysis.R
@@ -0,0 +1,60 @@
+# Analysis of Pancreatic cancer dataset using CNAmet
+library(CNAmet)
+library(tictoc)
+rm(list=ls())
+
+# load data
+load("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/PancreaticCancerRawDataset.Rdata")
+rm(paad.me)
+
+### reading copy number values and creating two binary matrices for amplification and deletion calls
+cn.amp <- paad.cnv
+cn.amp[cn.amp>1] <- 1
+cn.amp[cn.amp<0] <- 0
+
+cn.del <- paad.cnv
+cn.del[cn.del>0] <- 0
+cn.del[cn.del<0] <- 1
+cn.del[1:5,1:5]
+
+cn.amp <- as.matrix(cn.amp, rownames.force = T)
+cn.del <- as.matrix(cn.del, rownames.force = T)
+ge.exp <- as.matrix(paad.ge, rownames.force = T)
+
+# running CNAmet
+tic("Total")
+tic("Amplification")
+ampResults <- CNAmet(exprMatrix = ge.exp, cghMatrix = cn.amp, methylMatrix = NULL, perms = 1000, na.limit = 0.1, gainData = TRUE,
+                     favorSynergetic = TRUE, strictChecks = FALSE, strictLim = 0.05)
+toc()
+tic("Deletion")
+delResults <- CNAmet(exprMatrix = ge.exp, cghMatrix = cn.del, methylMatrix = NULL, perms = 1000, na.limit = 0.1, gainData = FALSE,
+                     favorSynergetic = TRUE, strictChecks = FALSE, strictLim = 0.05)
+toc()
+toc()
+
+# > ampResults <- CNAmet(exprMatrix = ge.exp, cghMatrix = cn.amp, methylMatrix = NULL, perms = 1000, na.limit = 0.1, gainData = TRUE,
+#                        +                      favorSynergetic = TRUE, strictChecks = FALSE, strictLim = 0.05)
+# > toc()
+# Amplification: 1620.831 sec elapsed
+# > tic("Deletion")
+# > delResults <- CNAmet(exprMatrix = ge.exp, cghMatrix = cn.del, methylMatrix = NULL, perms = 1000, na.limit = 0.1, gainData = FALSE,
+#                        +                      favorSynergetic = TRUE, strictChecks = FALSE, strictLim = 0.05)
+# > toc()
+# Deletion: 1694.469 sec elapsed
+# > toc()
+# Total: 3315.302 sec elapsed
+
+ampResults <- data.frame(ampResults)
+delResults <- data.frame(delResults)
+ampResults$Gene <- rownames(ampResults)
+delResults$Gene <- rownames(delResults)
+
+# saving results
+setwd("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/CNAmet/")
+write.table(ampResults, file = "CNAmet_CompPAAD_GainDriven_Genes.tsv", row.names = F, sep = "\t", quote = F)
+write.table(delResults, file = "CNAmet_CompPAAD_LossDriven_Genes.tsv", row.names = F, sep = "\t", quote = F)
+
+
+
+
