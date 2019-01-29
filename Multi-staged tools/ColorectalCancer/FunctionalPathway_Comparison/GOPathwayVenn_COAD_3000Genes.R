bdee0c33778cd84b76294b99caa1451b6058d2f0
AtinaSat <45379687+AtinaSat@users.noreply.github.com>
2019-01-29 18:12:01 +1000

    Update GOPathwayVenn_COAD_3000Genes.R

--- Multi-staged tools/ColorectalCancer/FunctionalPathway_Comparison/GOPathwayVenn_COAD_3000Genes.R
+++ Multi-staged tools/ColorectalCancer/FunctionalPathway_Comparison/GOPathwayVenn_COAD_3000Genes.R
@@ -8,34 +8,33 @@ rm(list = ls())
 
 ## Biological Process (BP) terms Venn analysis
 # Reading the files
-setwd("/home/anita/Benchmarking/two_omics/ColonCancerCompleteDataAnalysis/")
 
 # cancer genes BP terms
-cancerBP <- read.delim("Venns/DAVID/CancerGenes_GOTERM_BP_FAT.txt",
+cancerBP <- read.delim("DAVID/CancerGenes_GOTERM_BP_FAT.txt",
                        header = T, sep = "\t")
 cancerBP <- cancerBP[cancerBP$FDR<0.05,]
 cancerBP <- as.character(cancerBP$Term)
 
 # CNAmet genes BP terms
-CNAmetBP <- read.delim("CNAmet/DAVID/CNAmet_CompCOAD_3000Genes_GOTERM_BP_FAT.txt",
+CNAmetBP <- read.delim("ColorectalCancer/CNAmet/DAVID/CNAmet_COAD_3000Genes_GOTERM_BP_FAT.txt",
                        header = T, sep = "\t")
 CNAmetBP <- CNAmetBP[CNAmetBP$FDR<0.05,]
 CNAmetBP <- as.character(CNAmetBP$Term)
 
 #iGC genes BP terms
-iGCBP <- read.delim("iGC/DAVID/iGC_CompCOAD_3000Genes_GOTERM_BP_FAT.txt",
+iGCBP <- read.delim("ColorectalCancer/iGC/DAVID/iGC_COAD_3000Genes_GOTERM_BP_FAT.txt",
                     header = T, sep = "\t")
 iGCBP <- iGCBP[iGCBP$FDR<0.05,]
 iGCBP <- as.character(iGCBP$Term)
 
 # PLRS genes BP terms
-plrsBP <- read.delim("plrs/DAVID/PLRS_CompCOAD_3000Genes_GOTERM_BP_FAT.txt",
+plrsBP <- read.delim("ColorectalCancer/PLRS/DAVID/PLRS_COAD_3000Genes_GOTERM_BP_FAT.txt",
                      header = T, sep = "\t")
 plrsBP <- plrsBP[plrsBP$FDR<0.05,]
 plrsBP <- as.character(plrsBP$Term)
 
 # Oncodrive-CIS genes BP terms
-OncoBP <- read.delim("OncodriveCIS/DAVID/Oncodrive_CompCOAD_3000Genes_GOTERM_BP_FAT.txt",
+OncoBP <- read.delim("ColorectalCancer/OncodriveCIS/DAVID/Oncodrive_COAD_3000Genes_GOTERM_BP_FAT.txt",
                      header = T, sep = "\t")
 OncoBP <- OncoBP[OncoBP$FDR<0.05,]
 OncoBP <- as.character(OncoBP$Term)
@@ -47,7 +46,6 @@ cancerplrsBP <- intersect(cancerBP, plrsBP)
 cancerOncoBP <- intersect(cancerBP, OncoBP)
 
 # saving the venn diagram
-setwd("/home/anita/Benchmarking/two_omics/ColonCancerCompleteDataAnalysis/Venns/")
 jpeg("COAD_3000BPVenn.jpg", width = 1260, height = 890) 
 venn(list(CNAmet = CNAmetBP,  iGC = iGCBP,  PLRS = plrsBP,
           OncodriveCIS = OncoBP,
@@ -58,34 +56,32 @@ dev.off()
 ####################################################################################################
 ## Cellular Components (CC) terms Venn analysis
 
-setwd("/home/anita/Benchmarking/two_omics/ColonCancerCompleteDataAnalysis/")
-
 # Cancer genes
-cancerCC <- read.delim("Venns/DAVID/CancerGenes_GOTERM_CC_FAT.txt",
+cancerCC <- read.delim("DAVID/CancerGenes_GOTERM_CC_FAT.txt",
                        header = T, sep = "\t")
 cancerCC <- cancerCC[cancerCC$FDR<0.05,]
 cancerCC <- as.character(cancerCC$Term)
 
 # CNAmet
-CNAmetCC <- read.delim("CNAmet/DAVID/CNAmet_CompCOAD_3000Genes_GOTERM_CC_FAT.txt",
+CNAmetCC <- read.delim("ColorectalCancer/CNAmet/DAVID/CNAmet_COAD_3000Genes_GOTERM_CC_FAT.txt",
                        header = T, sep = "\t")
 CNAmetCC <- CNAmetCC[CNAmetCC$FDR<0.05,]
 CNAmetCC <- as.character(CNAmetCC$Term)
 
 # iGC
-iGCCC <- read.delim("iGC/DAVID/iGC_CompCOAD_3000Genes_GOTERM_CC_FAT.txt",
+iGCCC <- read.delim("ColorectalCancer/iGC/DAVID/iGC_COAD_3000Genes_GOTERM_CC_FAT.txt",
                     header = T, sep = "\t")
 iGCCC <- iGCCC[iGCCC$FDR<0.05,]
 iGCCC <- as.character(iGCCC$Term)
 
 # PLRS
-plrsCC <- read.delim("plrs/DAVID/PLRS_CompCOAD_3000Genes_GOTERM_CC_FAT.txt",
+plrsCC <- read.delim("ColorectalCancer/PLRS/DAVID/PLRS_COAD_3000Genes_GOTERM_CC_FAT.txt",
                      header = T, sep = "\t")
 plrsCC <- plrsCC[plrsCC$FDR<0.05,]
 plrsCC <- as.character(plrsCC$Term)
 
 # Oncodrive-CIS
-OncoCC <- read.delim("OncodriveCIS/DAVID/Oncodrive_CompCOAD_3000Genes_GOTERM_CC_FAT.txt",
+OncoCC <- read.delim("ColorectalCancer/OncodriveCIS/DAVID/Oncodrive_COAD_3000Genes_GOTERM_CC_FAT.txt",
                      header = T, sep = "\t")
 OncoCC <- OncoCC[OncoCC$FDR<0.05,]
 OncoCC <- as.character(OncoCC$Term)
@@ -97,7 +93,6 @@ cancerplrsCC <- intersect(cancerCC, plrsCC)
 cancerOncoCC <- intersect(cancerCC, OncoCC)
 
 # saving the Venn diagram
-setwd("/home/anita/Benchmarking/two_omics/ColonCancerCompleteDataAnalysis/Venns/")
 jpeg("COAD_3000CCVenn.jpg", width = 1260, height = 890) 
 venn(list(CNAmet = CNAmetCC,  iGC = iGCCC,  PLRS = plrsCC,
           OncodriveCIS = OncoCC,
@@ -108,34 +103,32 @@ dev.off()
 ########################################################################################################
 # Molecular Functional (MF) Venn analysis
 
-setwd("/home/anita/Benchmarking/two_omics/ColonCancerCompleteDataAnalysis/")
-
 # cancer
-cancerMF <- read.delim("Venns/DAVID/CancerGenes_GOTERM_MF_FAT.txt",
+cancerMF <- read.delim("DAVID/CancerGenes_GOTERM_MF_FAT.txt",
                        header = T, sep = "\t")
 cancerMF <- cancerMF[cancerMF$FDR<0.05,]
 cancerMF <- as.character(cancerMF$Term)
 
 # CNAmet
-CNAmetMF <- read.delim("CNAmet/DAVID/CNAmet_CompCOAD_3000Genes_GOTERM_MF_FAT.txt",
+CNAmetMF <- read.delim("ColorectalCancer/CNAmet/DAVID/CNAmet_COAD_3000Genes_GOTERM_MF_FAT.txt",
                        header = T, sep = "\t")
 CNAmetMF <- CNAmetMF[CNAmetMF$FDR<0.05,]
 CNAmetMF <- as.character(CNAmetMF$Term)
 
 # iGC
-iGCMF <- read.delim("iGC/DAVID/iGC_CompCOAD_3000Genes_GOTERM_MF_FAT.txt",
+iGCMF <- read.delim("ColorectalCancer/iGC/DAVID/iGC_COAD_3000Genes_GOTERM_MF_FAT.txt",
                     header = T, sep = "\t")
 iGCMF <- iGCMF[iGCMF$FDR<0.05,]
 iGCMF <- as.character(iGCMF$Term)
 
 # PLRS
-plrsMF <- read.delim("plrs/DAVID/PLRS_CompCOAD_3000Genes_GOTERM_MF_FAT.txt",
+plrsMF <- read.delim("ColorectalCancer/PLRS/DAVID/PLRS_COAD_3000Genes_GOTERM_MF_FAT.txt",
                      header = T, sep = "\t")
 plrsMF <- plrsMF[plrsMF$FDR<0.05,]
 plrsMF <- as.character(plrsMF$Term)
 
 # Oncodrive-CIS
-OncoMF <- read.delim("OncodriveCIS/DAVID/Oncodrive_CompCOAD_3000Genes_GOTERM_MF_FAT.txt",
+OncoMF <- read.delim("ColorectalCancer/OncodriveCIS/DAVID/Oncodrive_COAD_3000Genes_GOTERM_MF_FAT.txt",
                      header = T, sep = "\t")
 OncoMF <- OncoMF[OncoMF$FDR<0.05,]
 OncoMF <- as.character(OncoMF$Term)
@@ -147,7 +140,6 @@ cancerplrsMF <- intersect(cancerMF, plrsMF)
 cancerOncoMF <- intersect(cancerMF, OncoMF)
 
 # saving venn diagrams
-setwd("/home/anita/Benchmarking/two_omics/ColonCancerCompleteDataAnalysis/Venns/")
 jpeg("COAD_3000MFVenn.jpg", width = 1260, height = 890) 
 venn(list(CNAmet = CNAmetMF,  iGC = iGCMF,  PLRS = plrsMF,
           OncodriveCIS = OncoMF,
@@ -158,33 +150,33 @@ dev.off()
 ####################################################################################################
 # Pathways Venn Analysis
 # Reading the files
-setwd("/home/anita/Benchmarking/two_omics/ColonCancerCompleteDataAnalysis/")
+
 # Cancer
-cancerPathway <- read.delim("Venns/DAVID/CancerGenes_Reactome_Pathway.csv",
+cancerPathway <- read.delim("DAVID/CancerGenes_Reactome_Pathway.csv",
                             header = T, sep = ",")
 cancerPathway <- cancerPathway[cancerPathway$Entities.FDR<0.05,]
 cancerPathway <- as.character(cancerPathway$Pathway.identifier)
 
 #CNAmet
-CNAmetPathway <- read.delim("CNAmet/DAVID/CNAmet_CompCOAD_3000Genes_Reactome_Pathway.csv",
+CNAmetPathway <- read.delim("ColorectalCancer/CNAmet/DAVID/CNAmet_COAD_3000Genes_Reactome_Pathway.csv",
                             header = T, sep = ",")
 CNAmetPathway <- CNAmetPathway[CNAmetPathway$Entities.FDR<0.05,]
 CNAmetPathway <- as.character(CNAmetPathway$Pathway.identifier)
 
 # iGC
-iGCPathway <- read.delim("iGC/DAVID/iGC_CompCOAD_3000Genes_Reactome_Pathway.csv",
+iGCPathway <- read.delim("ColorectalCancer/iGC/DAVID/iGC_COAD_3000Genes_Reactome_Pathway.csv",
                          header = T, sep = ",")
 iGCPathway <- iGCPathway[iGCPathway$Entities.FDR<0.05,]
 iGCPathway <- as.character(iGCPathway$Pathway.identifier)
 
 # PLRS
-plrsPathway <- read.delim("plrs/DAVID/PLRS_CompCOAD_3000Genes_Reactome_Pathway.csv",
+plrsPathway <- read.delim("ColorectalCancer/PLRS/DAVID/PLRS_COAD_3000Genes_Reactome_Pathway.csv",
                           header = T, sep = ",")
 plrsPathway <- plrsPathway[plrsPathway$Entities.FDR<0.05,]
 plrsPathway <- as.character(plrsPathway$Pathway.identifier)
 
 # Oncodrive-CIS
-OncoPathway <- read.delim("OncodriveCIS/DAVID/Oncodrive_CompCOAD_3000Genes_Reactome_Pathway.csv",
+OncoPathway <- read.delim("ColorectalCancer/OncodriveCIS/DAVID/Oncodrive_COAD_3000Genes_Reactome_Pathway.csv",
                           header = T, sep = ",")
 OncoPathway <- OncoPathway[OncoPathway$Entities.FDR<0.05,]
 OncoPathway <- as.character(OncoPathway$Pathway.identifier)
@@ -196,7 +188,6 @@ cancerplrsPathway <- intersect(cancerPathway, plrsPathway)
 cancerOncoPathway <- intersect(cancerPathway, OncoPathway)
 
 # Saving the venn diagram
-setwd("/home/anita/Benchmarking/two_omics/ColonCancerCompleteDataAnalysis/Venns/")
 jpeg("COAD_3000PathwayVenn.jpg", width = 1260, height = 890) 
 venn(list(CNAmet = CNAmetPathway,  iGC = iGCPathway,  PLRS = plrsPathway,
           OncodriveCIS = OncoPathway,
