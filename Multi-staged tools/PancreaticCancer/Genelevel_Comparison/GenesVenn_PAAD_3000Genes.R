6c5a923887cf24ad375eb867ac8a81ee40a38a5a
AtinaSat <45379687+AtinaSat@users.noreply.github.com>
2019-01-29 15:35:20 +1000

    Update GenesVenn_PAAD_3000Genes.R

--- Multi-staged tools/PancreaticCancer/Genelevel_Comparison/GenesVenn_PAAD_3000Genes.R
+++ Multi-staged tools/PancreaticCancer/Genelevel_Comparison/GenesVenn_PAAD_3000Genes.R
@@ -11,8 +11,7 @@ rm(list=ls())
 # hence, perform fdr correction and select top 1500 amplification driven and top 1500 deletion driven genes based on fdr
 
 # amplification driven genes
-CNAmetAmp <- read.delim("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/CNAmet/CNAmet_CompPAAD_GainDriven_Genes.tsv",
-                        header = T, sep = "\t")
+CNAmetAmp <- read.delim("PancreaticCancer/CNAmet/CNAmet_PAAD_GainDriven_Genes.tsv", header = T, sep = "\t")
 CNAmetAmp <- CNAmetAmp[,c(3,4,10)]
 CNAmetAmp <- na.omit(CNAmetAmp)
 CNAmetAmp$p.adj.fdr <- p.adjust(CNAmetAmp$CWPvalue, method = "fdr")
@@ -21,8 +20,7 @@ CNAmetAmp <- CNAmetAmp[order(CNAmetAmp$p.adj.fdr),]
 CNAmetAmpSelected <- CNAmetAmp[1:1500,]
 
 # deletion driven genes
-CNAmetDel <- read.delim("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/CNAmet/CNAmet_CompPAAD_LossDriven_Genes.tsv",
-                        header = T, sep = "\t")
+CNAmetDel <- read.delim("PancreaticCancer/CNAmet/CNAmet_PAAD_LossDriven_Genes.tsv",header = T, sep = "\t")
 CNAmetDel <- CNAmetDel[,c(3,4,10)]
 CNAmetDel <- na.omit(CNAmetDel)
 CNAmetDel$p.adj.fdr <- p.adjust(CNAmetDel$CWPvalue, method = "fdr")
@@ -37,28 +35,24 @@ CNAmetBoth <- intersect(as.character(CNAmetAmpSelected$Gene), as.character(CNAme
 CNAmetTotal <- unique(c(as.character(CNAmetAmpSelected$Gene), as.character(CNAmetDelSelected$Gene)))
 
 # save genes
-write.table(CNAmetTotal, file = "/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/CNAmet/CNAmet_CompPAAD_3000Genes.txt",
-            row.names = F, quote = F, sep = "\t")
+# write.table(CNAmetTotal, file = "CNAmet/CNAmet_PAAD_3000Genes.txt", row.names = F, quote = F, sep = "\t")
 
 ####################################################################################################################
 ## iGC
 # read the results and select top 1500 amplification driven and 1500 deletion driven genes based on fdr
 
 # amplification driven genes
-iGCAmp <- read.delim("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/iGC/CNA_GainDriven_genes.tsv",
-                     header = T, sep = "\t")
+iGCAmp <- read.delim("PancreaticCancer/iGC/iGC_PAAD_GainDriven_genes.tsv",header = T, sep = "\t")
 iGCAmp <- iGCAmp[,c(1,3)]
 iGCAmp <- iGCAmp[iGCAmp$fdr<0.05,]
 
 # deletion driven genes
-iGCDel <- read.delim("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/iGC/CNA_LossDriven_genes.tsv",
-                     header = T, sep = "\t")
+iGCDel <- read.delim("PancreaticCancer/iGC/iGC_PAAD_LossDriven_genes.tsv",header = T, sep = "\t")
 iGCDel <- iGCDel[,c(1,3)]
 iGCDel <- iGCDel[iGCDel$fdr<0.05,]
 
 # Both genes
-iGCBoth <- read.delim("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/iGC/CNA_BothDriven_genes.tsv",
-                      header = T, sep = "\t")
+iGCBoth <- read.delim("PancreaticCancer/iGC/CNA_BothDriven_genes.tsv",header = T, sep = "\t")
 iGCBothAmp <- iGCBoth[(iGCBoth$gain_fdr<0.05),]
 iGCBothAmp <- iGCBothAmp[,c(1,3)]
 colnames(iGCBothAmp)[2] <- "fdr"
@@ -78,24 +72,21 @@ iGCBoth <- intersect(as.character(iGCAmpSelected$GENE), as.character(iGCDelSelec
 iGCTotal <- unique(c(as.character(iGCAmpSelected$GENE), as.character(iGCDelSelected$GENE)))
 
 # save genes
-write.table(iGCTotal, file = "/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/iGC/iGC_CompPAAD_3000Genes.txt",
-            row.names = F, quote = F, sep = "\t")
+write.table(iGCTotal, file = "PancreaticCancer/iGC/iGC_PAAD_3000Genes.txt", row.names = F, quote = F, sep = "\t")
 
 ####################################################################################################################
 ## plrs
 # plrs does not output amplfied and deletion driven genes. Hence selected top 3000 genes from results based on fdr
 
 # total genes
-plrsGenes <- read.delim("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/plrs/PLRS_PAAD_Results.tsv",
-                        header = T, sep = "\t")
+plrsGenes <- read.delim("PancreaticCancer/PLRS/PLRS_PAAD_Results.tsv", header = T, sep = "\t")
 plrsGenes<- plrsGenes[plrsGenes$BH.adj.pval<0.05,] 
 plrsGenes <- plrsGenes[order(plrsGenes$BH.adj.pval),]
 plrsSelected <- plrsGenes[1:3000,]
 plrsTotal <- as.character(plrsSelected$Gene)
 
 # save genes
-write.table(plrsTotal, file = "/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/plrs/PLRS_CompPAAD_3000Genes.txt",
-            row.names = F, quote = F, sep = "\t")
+write.table(plrsTotal, file = "PancreaticCancer/PLRS/PLRS_CompPAAD_3000Genes.txt", row.names = F, quote = F, sep = "\t")
 
 ####################################################################################################################
 ## Oncodrive-CIS
@@ -103,7 +94,7 @@ write.table(plrsTotal, file = "/home/anita/Benchmarking/two_omics/PancreaticCanc
 # select top 1500 amplification driven and 1500 deletion driven gene based on fdr
 
 # amplification driven genes
-OncoAmp <- read.delim("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/OncodriveCIS/CompPAAD.Output/OncoCNA.AMP",
+OncoAmp <- read.delim("PancreaticCancer/OncodriveCIS/OncoCNA.AMP",
                       header = T, sep = "\t")
 OncoAmp$pval <- pnorm(-abs(OncoAmp$Z.COMBINED))
 OncoAmp$p.adj.fdr <- p.adjust(OncoAmp$pval, method = "fdr")
@@ -111,7 +102,7 @@ OncoAmp <- OncoAmp[OncoAmp$Z.COMBINED>0,]
 OncoAmpSelected <- OncoAmp[OncoAmp$p.adj.fdr<0.05,] # less than 1500.. so using all
 
 # deletion driven genes
-OncoDel <- read.delim("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/OncodriveCIS/CompPAAD.Output/OncoCNA.DEL",
+OncoDel <- read.delim("PancreaticCancer/OncodriveCIS/OncoCNA.DEL",
                       header = T, sep = "\t")
 head(OncoDel)
 tail(OncoDel)
@@ -127,7 +118,7 @@ OncoBoth <- intersect(as.character(OncoAmpSelected$ENS.ID), as.character(OncoDel
 OncoTotal <- unique(c(as.character(OncoAmpSelected$ENS.ID), as.character(OncoDelSelected$ENS.ID)))
 
 # save genes
-write.table(OncoTotal, file = "/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/OncodriveCIS/Oncodrive_CompPAAD_3000Genes.txt",
+write.table(OncoTotal, file = "PancreaticCancer/OncodriveCIS/Oncodrive_PAAD_3000Genes.txt",
             row.names = F, quote = F, sep = "\t")
 
 ####################################################################################################################
@@ -136,7 +127,7 @@ write.table(OncoTotal, file = "/home/anita/Benchmarking/two_omics/PancreaticCanc
 
 library(readr)
 # reading the text file
-mystring <- read_file("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/Venns/cBioPortalCancerGeneList.txt")
+mystring <- read_file("PancreaticCancer/Genelevel_Comparison/cBioPortalCancerGeneList.txt")
 cancergenes <- strsplit(mystring, "\n", fixed = FALSE, perl = FALSE, useBytes = FALSE)[[1]]
 
 # finding common genes between tools and cancer gene list
@@ -153,7 +144,7 @@ venn(list(CNAmet = CNAmetTotal,  iGC = iGCTotal,  PLRS = plrsTotal,
      zcolor = "style", cexil = 1.5, cexsn = 1.5, opacity = 0.3,size = 20)
 
 ## Save the venn plot
-setwd("/home/anita/Benchmarking/two_omics/PancreaticCancerCompleteDataAnalysis/Venns/")
+setwd("PancreaticCancer/Genelevel_Comprison")
 jpeg("PAAD_3000GenesVenn.jpg", width = 1260, height = 890)  
 venn(list(CNAmet = CNAmetTotal,  iGC = iGCTotal,  PLRS = plrsTotal,
           OncodriveCIS = OncoTotal,
